05fc166eeb6c9eeb9fa8b13f596089e4079c6c52
denisivan0v <denis@ivanovdenis.ru>
2018-06-04 15:05:03 +0700

    recent changes

---
+++ src/Broadway.DataProjection/DataProjectionContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using NuClear.Broadway.Interfaces.Models;
+
+namespace NuClear.Broadway.DataProjection
+{
+    public class DataProjectionContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<SecondRubric> SecondRubrics { get; set; }
+        public DbSet<Rubric> Rubrics { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var category = modelBuilder.Entity<Category>();
+            var secondRubric = modelBuilder.Entity<SecondRubric>();
+            var rubric = modelBuilder.Entity<Rubric>();
+
+            category.HasKey(x => x.Code);
+            category.Property(x => x.IsDeleted).IsRequired();
+
+            secondRubric.HasKey(x => x.Code);
+            secondRubric.Property(x => x.IsDeleted).IsRequired();
+
+            rubric.HasKey(x => x.Code);
+            rubric.Property(x => x.Code).IsRequired();
+        }
+    }
+}
---
+++ src/Broadway.DataProjection/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Broadway.DataProjection
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
